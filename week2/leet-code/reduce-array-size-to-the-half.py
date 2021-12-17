747af0cbe71070d563f7103fbff1cae30d283afb
BiniyamAb <se.biniyam.abiy@gmail.com>
2021-12-17 08:58:47 +0300

    reduce-array-to-the-half[leet-code]

--- week2/leet-code/reduce-array-size-to-the-half.py
+++ week2/leet-code/reduce-array-size-to-the-half.py
@@ -1,3 +1,22 @@
 from typing import List
-# 7:35
+
 def minSetSize(arr: List[int]) -> int:
+    if len(set(arr)) == 1:
+            return 1
+    
+    freqDict, size, sumOf = {}, 0, 0
+    
+    for i in range(len(arr)):
+        freqDict[arr[i]] = freqDict.get(arr[i], 0) + 1
+    
+    lst = list(freqDict.values())
+    lst.sort(reverse=True)
+    
+    for i in range(len(lst)):
+        sumOf+=lst[i]
+        size+=1
+        
+        if sumOf >= len(arr) // 2:
+            break
+                    
+    return size
