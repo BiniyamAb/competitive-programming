1b8f3617a941abc79e9811a119b2943369fc407a
BiniyamAb <se.biniyam.abiy@gmail.com>
2021-12-18 12:17:22 +0300

    valid-parentheses[leet-code] revision 1

--- week2/leet-code/valid-parentheses.py
+++ week2/leet-code/valid-parentheses.py
@@ -1,32 +1,17 @@
 def isValid(s: str) -> bool:
     lst = []
-    if len(s) % 2 != 0:
-        return False
+    if len(s) % 2 != 0: return False
+    tags = {"{": "}", "[": "]", "(": ")"}
     for x in s:
         if x in "({[":
             lst.append(x)
         elif x in ")}]":
             if lst:
-                if lst[-1] == "(":
-                    if x == ")":
-                        lst.pop()
-                    else:
-                        return False
-                elif lst[-1] == "{":
-                    if x == "}":
-                        lst.pop()
-                    else:
-                        return False
-                elif lst[-1] == "[":
-                    if x == "]":
-                        lst.pop()
-                    else:
-                        return False
+                if x == tags[lst[-1]]:
+                    lst.pop()
                 else:
                     return False
             else:
                 return False
-    if lst:
-        return False
-    else:
-        return True
+            
+    return False if lst else True
