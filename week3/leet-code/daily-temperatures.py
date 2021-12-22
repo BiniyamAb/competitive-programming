6c28055f270cce6534a1bcf6a2da2df43e275f4d
BiniyamAb <se.biniyam.abiy@gmail.com>
2021-12-22 18:32:39 +0300

    daily-temperatures[leet-code]

---
+++ week3/leet-code/daily-temperatures.py
@@ -0,0 +1,19 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        monoStack = []
+        res = [0] * len(temperatures)
+        
+        for i in range(len(temperatures)):
+        
+            while len(monoStack) > 0 and temperatures[i] > temperatures[monoStack[-1]]:
+                res[monoStack[-1]] = i - monoStack[-1]
+                monoStack.pop()
+                
+            monoStack.append(i)
+        
+        for i in range(len(monoStack)):
+            res[monoStack[i]] = 0
+        
+        return res
+            
+        
