f11e4cc2d9ed43923a1ce83d37d2fb4ec250a779
BiniyamAb <se.biniyam.abiy@gmail.com>
2022-01-25 10:03:45 +0300

    [leet-code] delete-node-in-linked-list --v2

--- week5/leet-code/delete-node-in-a-linked-list.py
+++ week5/leet-code/delete-node-in-a-linked-list.py
@@ -9,11 +9,5 @@ class Solution:
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
-        head = node
-        while(head.next):
-            head.val = head.next.val
-            if head.next.next == None:
-                break
-            head = head.next
-        head.next = None
-        return
+        node.val = node.next.val
+        node.next = node.next.next
