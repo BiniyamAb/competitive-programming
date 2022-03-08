0fb8ef3dc6097028eb0de1507a7a3eee9103a72c
BiniyamAb <se.biniyam.abiy@gmail.com>
2022-03-08 17:36:19 +0300

    [leet-code] boats-to-save-people

---
+++ camp/week3/boats-to-save-people.py
@@ -0,0 +1,16 @@
+from ast import List
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        l, r = 0, len(people) - 1
+        boats = 0
+        people.sort()
+        while l <= r:
+            if people[l] + people[r] > limit:
+                r -= 1
+            else:
+                r -= 1
+                l += 1
+            boats += 1
+        
+        return boats
